2f949a96dd3ea2093ccee159f184eeb00b9a3805
ChesterHan <chestermongo2026@gmail.com>
2025-01-04 11:58:00 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <main className='overflow_hidden'>
+        <Header></Header>
+        <Hero></Hero>
+        <Features />
+
+    </main>
+    )
+}
+
+export default App
